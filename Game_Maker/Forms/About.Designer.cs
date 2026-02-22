cc236a33a304d158d5e99381c421430a539a3171
latenightbuddha <latenightbuddha@users.noreply.github.com>
2026-02-22 13:54:38 -0500

    Foundation, First Commit

---
+++ Game_Maker/Forms/About.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Game_Maker
+{
+    partial class About
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            tableLayoutPanel1 = new TableLayoutPanel();
+            richTextBox1 = new RichTextBox();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            Button_OK = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.FromArgb(64, 64, 64);
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 56.5F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 43.5F));
+            tableLayoutPanel1.Controls.Add(richTextBox1, 0, 1);
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel1, 0, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 31.5245476F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 68.47545F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.Size = new Size(369, 387);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = Color.FromArgb(64, 64, 64);
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.Dock = DockStyle.Right;
+            richTextBox1.ForeColor = Color.Silver;
+            richTextBox1.Location = new Point(3, 112);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ScrollBars = RichTextBoxScrollBars.None;
+            richTextBox1.Size = new Size(363, 231);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(64, 64, 64);
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(363, 103);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(64, 64, 64);
+            panel1.Controls.Add(Button_OK);
+            panel1.Dock = DockStyle.Fill;
+            panel1.ForeColor = Color.Silver;
+            panel1.Location = new Point(3, 349);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(363, 35);
+            panel1.TabIndex = 3;
+            // 
+            // Button_OK
+            // 
+            Button_OK.ForeColor = Color.Black;
+            Button_OK.Location = new Point(284, 7);
+            Button_OK.Name = "Button_OK";
+            Button_OK.Size = new Size(75, 23);
+            Button_OK.TabIndex = 0;
+            Button_OK.Text = "OK";
+            Button_OK.UseVisualStyleBackColor = true;
+            // 
+            // About
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(369, 387);
+            Controls.Add(tableLayoutPanel1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "About";
+            Text = "About";
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private RichTextBox richTextBox1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button Button_OK;
+    }
+}
---
+++ Game_Maker/Forms/About.cs
@@ -0,0 +1,10 @@
+namespace Game_Maker
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+    }
+}
